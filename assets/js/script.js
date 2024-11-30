1d6dcc22f2bdd5cea074e9b22e365f885887fae3
AbdulHaq56 <abdulhaqbfk0322@gmail.com>
2024-11-30 13:19:41 +0500

    css update

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ if (uName == null || uName == "") {
   if (uAge == null || uAge == "") {
     alert("Please Enter all Details");
     location.assign("index.html");
-  } else if (isNaN(uAge) || uAge <= 0 ) {
+  } else if (isNaN(uAge) || uAge <= 0) {
     alert("Please enter a valid number for your Age");
     location.assign("index.html");
   } else {
@@ -19,13 +19,24 @@ if (uName == null || uName == "") {
       let printConfirmation = confirm(
         `Do you want to print all details or Only Name?`
       );
-
       if (printConfirmation) {
         document.write(
-          `<div><p class="title">Your Details</p><div class="details"><p>Name:</p> <h2> ${uName}</h2> <br><p>Age:</p> <h2> ${uAge}</h2> <br><p>Qualification:</p>  <h2> ${uQualification}</h2></div></div>`
+          `<div>
+          <p class="title">Your Details</p>
+          <div class="details">
+          <p>Name:</p> <h2> ${uName}</h2> <br>
+          <p>Age:</p> <h2> ${uAge}</h2> <br>
+          <p>Qualification:</p>  <h2> ${uQualification}</h2>
+          </div>
+          </div>`
         );
       } else {
-        document.write(`Name: ${uName}`);
+        document.write(`
+          <p class="title">Your Details</p>
+          <div class="details">
+          <p>Name:</p> <h2> ${uName}</h2>
+          </div>
+          `);
       }
     }
   }
